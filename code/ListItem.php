465110c80d33d23d3cbc7ef2cb9c54eba146f4b9
Andrew Houle <andrewhoule@gmail.com>
2015-11-03 12:25:25 -0500

    Clean up code and better list item logic

--- code/ListItem.php
+++ code/ListItem.php
@@ -1,135 +1,139 @@
 <?php
 
 class ListItem extends DataObject { 
-	
-	private static $db = array (
-		'SortID' => 'Int',
-		'Title' => 'Text',
-		'Content' => 'HTMLText',
-		'Link' => 'Text'
-	);
-	
-	private static $has_one = array (
-		'ListPage' => 'ListPage',
-		'ListCategory' => 'ListCategory',
-		'Photo' => 'Image',
-		'Resource' => 'File'
-	);
+  
+  private static $db = array (
+    'SortID' => 'Int',
+    'Title' => 'Text',
+    'Content' => 'HTMLText',
+    'LinkType' => "Enum('Link, Resource', 'Link')",
+    'Link' => 'Text'
+  );
+  
+  private static $has_one = array (
+    'ListPage' => 'ListPage',
+    'ListCategory' => 'ListCategory',
+    'Photo' => 'Image',
+    'Resource' => 'File'
+  );
 
-	private static $summary_fields = array (
-		'Thumbnail' => 'Photo',
-		'Title' => 'Title',
-		'Link' => 'Link',
-		'ContentExcerpt' => 'Content',
-		'Category' => 'Category'
-   );
+  private static $summary_fields = array (
+    'Thumbnail' => 'Photo',
+    'Title' => 'Title',
+    'Link' => 'Link',
+    'ContentExcerpt' => 'Content',
+    'Category' => 'Category'
+  );
 
-   public function canCreate($Member = null) { return true; }
-	public function canEdit($Member = null) { return true; }
-	public function canView($Member = null) { return true; }
-	public function canDelete($Member = null) { return true; }
+  private static $defaults = array (
+    'LinkType' => 'Link'
+  );
 
-	private static $default_sort = 'SortID Asc';
+  public function canCreate($Member = null) { return true; }
+  public function canEdit($Member = null) { return true; }
+  public function canView($Member = null) { return true; }
+  public function canDelete($Member = null) { return true; }
 
-	public function getCMSFields() {
-		if($this->ID == 0) {
-			$categorydropdown = TextField::create('CategoryDisclaimer')->setTitle('Category')->setDisabled(true)->setValue('You can assign a category once you have saved the record for the first time.');
-		}
-		else {
-			$categories = ListCategory::get()->filter("ListPageID","$this->ListPageID")->sort("Category ASC");
-			$map = $categories ? $categories->map('ID', 'Category', 'Please Select') : array();
-			if($map) {
-				$categorydropdown = new DropdownField('ListCategoryID','Category', $map);
-				$categorydropdown->setEmptyString("-- Please Select --");
-			}
-			else {
-				$categorydropdown = new DropdownField('ListCategoryID','Category', $map);
-				$categorydropdown->setEmptyString("There are no categories created yet"); 
-			}
-		}
-		$ImageField = UploadField::create('Photo')->setDescription('(Allowed filetypes: jpg, jpeg, png, gif)');
-		$ImageField->folderName = "ListPage"; 
-		$ImageField->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
-		$DocumentField = UploadField::create('Resource')->setTitle('Resource/Document')->setDescription('(Allowed filetypes: pdf, doc, docx, txt, ppt, or pptx)');
-		$DocumentField->folderName = "ListPage"; 
-		$DocumentField->getValidator()->allowedExtensions = array('pdf','doc','docx','txt','ppt','pptx');
-		return new FieldList(
-			$categorydropdown,
-			TextField::create("Title"),
-			OptionSetField::create("LinkType", "", array('link' => 'Link to a Page', 'resource' => 'Link to a Resource/Document')),
-			TextField::create("Link")
-				->displayIf("LinkType")->isEqualTo("link")->andIf("LinkType")->isNotEqualTo("resource")
-            	->end(),
-            $DocumentField
-				->displayIf("LinkType")->isEqualTo("resource")->andIf("LinkType")->isNotEqualTo("link")
-            	->end(),
-			$ImageField,
-			HTMLEditorField::create("Content")
-		);
-	}
+  private static $default_sort = 'SortID Asc';
 
-	public function Category() {
-		$category = $this->ListCategory()->Category;
-		if($category != NULL) 
-			return $category;
-		else 
-			return "Other";
-	}
+  public function getCMSFields() {
+    if($this->ID == 0) {
+      $categorydropdown = TextField::create('CategoryDisclaimer')->setTitle('Category')->setDisabled(true)->setValue('You can assign a category once you have saved the record for the first time.');
+    }
+    else {
+      $categories = ListCategory::get()->filter("ListPageID","$this->ListPageID")->sort("Category ASC");
+      $map = $categories ? $categories->map('ID', 'Category', 'Please Select') : array();
+      if($map) {
+        $categorydropdown = new DropdownField('ListCategoryID','Category', $map);
+        $categorydropdown->setEmptyString("-- Please Select --");
+      }
+      else {
+        $categorydropdown = new DropdownField('ListCategoryID','Category', $map);
+        $categorydropdown->setEmptyString("There are no categories created yet"); 
+      }
+    }
+    $ImageField = UploadField::create('Photo')->setDescription('(Allowed filetypes: jpg, jpeg, png, gif)');
+    $ImageField->folderName = 'ListPage'; 
+    $ImageField->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
+    $DocumentField = UploadField::create('Resource')->setTitle('Resource/Document')->setDescription('(Allowed filetypes: pdf, doc, docx, txt, ppt, or pptx)');
+    $DocumentField->folderName = "ListPage"; 
+    $DocumentField->getValidator()->allowedExtensions = array('pdf','doc','docx','txt','ppt','pptx');
+    $LinkField = TextField::create('Link')->setTitle('Link URL');
+    $fields = FieldList::create(TabSet::create('Root'));
+    $fields->addFieldsToTab('Root.Main', array(
+      $categorydropdown,
+      TextField::create('Title'),
+      OptionSetField::create('LinkType')->setTitle('')->setSource($this->dbObject('LinkType')->enumValues()),
+      TextField::create('Link')->setTitle('Link URL')->displayIf('LinkType')->isEqualTo('Link')->andIf('LinkType')->isNotEqualTo('Resource')->end(),
+      DisplayLogicWrapper::create($DocumentField)->displayIf('LinkType')->isEqualTo('Resource')->andIf('LinkType')->isNotEqualTo('Link')->end(),
+      $ImageField,
+     HTMLEditorField::create('Content')
+    ));  
+    return $fields;
+  }
 
-	public function Thumbnail() {
-		$Image = $this->Photo();
-		if ( $Image ) 
-			return $Image->CMSThumbnail();
-		else 
-			return null;
-	}
-	
-	public function ContentExcerpt($length = 100) {
-   	$text = strip_tags($this->Content);
-	   $length = abs((int)$length);
-	   if(strlen($text) > $length) {
-	     	$text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
-	   }
-	   return $text;
-	}
+  public function Category() {
+    $category = $this->ListCategory()->Category;
+    if($category != NULL) 
+      return $category;
+    else 
+      return "Other";
+  }
 
-	public function PhotoSized() {
-		if($this->Photo()->exists()) {
-			$PhotoWidth = $this->Photo()->getWidth;
-			if($this->getComponent('ListPage')->PhotoMaxWidth) {
-				$MaxPhotoWidth = $this->getComponent('ListPage')->PhotoMaxWidth;
-			}
-			else {
-				$MaxPhotoWidth = 200;
-			}
-			if($PhotoWidth > $MaxPhotoWidth) {
-				return $this->Photo();
-			}
-			else {
-				return $this->Photo()->setWidth($MaxPhotoWidth);
-			}
-		}		
-	}
+  public function Thumbnail() {
+    $Image = $this->Photo();
+    if ( $Image ) 
+      return $Image->CMSThumbnail();
+    else 
+      return null;
+  }
+  
+  public function ContentExcerpt($length = 100) {
+    $text = strip_tags($this->Content);
+     $length = abs((int)$length);
+     if(strlen($text) > $length) {
+        $text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
+     }
+     return $text;
+  }
 
-	public function Link() {
-		if($this->Link) {
-			return $this->Link;
-		}
-		elseif($this->getComponent('Resource')->exists()) {
-			return $this->getComponent('Resource')->URL;
-		}
-		else
-			return false;
-	}
+  public function PhotoSized() {
+    if($this->Photo()->exists()) {
+      $PhotoWidth = $this->Photo()->getWidth;
+      if($this->getComponent('ListPage')->PhotoMaxWidth) {
+        $MaxPhotoWidth = $this->getComponent('ListPage')->PhotoMaxWidth;
+      }
+      else {
+        $MaxPhotoWidth = 200;
+      }
+      if($PhotoWidth > $MaxPhotoWidth) {
+        return $this->Photo();
+      }
+      else {
+        return $this->Photo()->setWidth($MaxPhotoWidth);
+      }
+    }   
+  }
 
-	public function LinkExists() {
-		if($this->Link || $this->getComponent('Resource')->exists()) 
-			return true;
-	}
+  public function Link() {
+    if($this->Link) {
+      return $this->Link;
+    }
+    elseif($this->getComponent('Resource')->exists()) {
+      return $this->getComponent('Resource')->URL;
+    }
+    else
+      return false;
+  }
 
-	public function ToggleEffectItems() {
-		if($this->getComponent('ListPage')->ToggleEffectItems == true)
-			return true;
-	}
+  public function LinkExists() {
+    if($this->Link || $this->getComponent('Resource')->exists()) 
+      return true;
+  }
+
+  public function ToggleEffectItems() {
+    if($this->getComponent('ListPage')->ToggleEffectItems == true)
+      return true;
+  }
 
 }
--- code/ListPage.php
+++ code/ListPage.php
@@ -2,137 +2,116 @@
 
 class ListPage extends Page {
     
-    private static $db = array (
-        'ToggleEffect' => 'Boolean',
-        'ToggleEffectItems' => 'Boolean',
-        'StartToggleClosed' => 'Boolean',
-        'AlphabeticalOrder' => 'Boolean',
-        'AlphaOrderCategories' => 'Boolean',
-        'BottomContent' => 'HTMLText',
-        'PhotoMaxWidth' => 'Int'
-    );
+  private static $db = array (
+    'ToggleEffect' => 'Boolean',
+    'ToggleEffectItems' => 'Boolean',
+    'StartToggleClosed' => 'Boolean',
+    'AlphabeticalOrder' => 'Boolean',
+    'AlphaOrderCategories' => 'Boolean',
+    'BottomContent' => 'HTMLText',
+    'PhotoMaxWidth' => 'Int'
+  );
     
-    private static $has_many = array (
-        'ListItems' => 'ListItem',
-        'ListCategories' => 'ListCategory'
-    );
+  private static $has_many = array (
+    'ListItems' => 'ListItem',
+    'ListCategories' => 'ListCategory'
+  );
 
-    private static $defaults = array (
-        'ToggleEffect' => true,
-        'ToggleEffectItems' => true,
-        'StartToggleClosed' => true,
-        'AlphabeticalOrder' => false,
-        'AlphaOrderCategories' => false,
-        'PhotoMaxWidth' => '300'
-    );
+  private static $defaults = array (
+    'ToggleEffect' => true,
+    'ToggleEffectItems' => true,
+    'StartToggleClosed' => true,
+    'AlphabeticalOrder' => false,
+    'AlphaOrderCategories' => false,
+    'PhotoMaxWidth' => '300'
+  );
     
-    private static $icon = "listpage/images/listpage";
-    
-    public function getCMSFields() {
-        $fields = parent::getCMSFields();
-        $fields->addFieldToTab('Root.Main', HTMLEditorField::create('BottomContent')->setTitle('Content for below the list items'),'Metadata');
-        $ListItemGridField = new GridField(
-            'ListItems',
-            'List Item',
-            $this->ListItems(),
-            GridFieldConfig::create()
-                ->addComponent(new GridFieldToolbarHeader())
-                ->addComponent(new GridFieldAddNewButton('toolbar-header-right'))
-                ->addComponent(new GridFieldSortableHeader())
-                ->addComponent(new GridFieldDataColumns())
-                ->addComponent(new GridFieldPaginator(50))
-                ->addComponent(new GridFieldEditButton())
-                ->addComponent(new GridFieldDeleteAction())
-                ->addComponent(new GridFieldDetailForm())
-                ->addComponent(new GridFieldFilterHeader())
-                ->addComponent($sortable=new GridFieldSortableRows('SortID'))
-        );
-        $sortable->setAppendToTop(true);
-        $fields->addFieldToTab("Root.ListItems", $ListItemGridField);
-        $ListCategoryGridField = new GridField(
-            'ListCategories',
-            'List Category',
-            $this->ListCategories(),
-            GridFieldConfig::create()
-                ->addComponent(new GridFieldToolbarHeader())
-                ->addComponent(new GridFieldAddNewButton('toolbar-header-right'))
-                ->addComponent(new GridFieldSortableHeader())
-                ->addComponent(new GridFieldDataColumns())
-                ->addComponent(new GridFieldPaginator(50))
-                ->addComponent(new GridFieldEditButton())
-                ->addComponent(new GridFieldDeleteAction())
-                ->addComponent(new GridFieldDetailForm())
-                ->addComponent(new GridFieldFilterHeader())
-                ->addComponent($sortable=new GridFieldSortableRows('SortID'))
-        );
-        $sortable->setAppendToTop(true);
-        $fields->addFieldToTab("Root.ListCategories", $ListCategoryGridField);
-        $fields->addFieldToTab('Root.Config', HeaderField::create('ListItemsDesc')->setTitle('List Items Options'));
-        $fields->addFieldToTab('Root.Config', CheckboxField::create('AlphabeticalOrder')->setTitle('Alphabetical Order for List Items')->setDescription('Show in alphabetical order (By the list item title, overwrites drag and drop order)'));
-        $fields->addFieldToTab('Root.Config', CheckboxField::create('ToggleEffectItems')->setTitle('Toggle Effect for List Items')->setDescription('Set toggle effect on list items'));
+  private static $icon = "listpage/images/listpage";
+   
+  public function getCMSFields() {
+    $fields = parent::getCMSFields();
+    $fields->addFieldToTab('Root.Main', HTMLEditorField::create('BottomContent')->setTitle('Content for below the list items'),'Metadata');
+    $fields->addFieldToTab('Root.ListItems', GridField::create(
+      'ListItems',
+      'List Items',
+      $this->ListItems(),
+      GridFieldConfig_RecordEditor::create('50')->addComponent($sortableitems = new GridFieldSortableRows('SortID'))
+    ));
+    $sortableitems->setAppendToTop(true);
+
+    $fields->addFieldToTab('Root.ListCategories', GridField::create(
+      'ListCategories',
+      'List Categories',
+      $this->ListCategories(),
+      GridFieldConfig_RecordEditor::create('50')->addComponent($sortablecategories = new GridFieldSortableRows('SortID'))
+    ));
+    $sortablecategories->setAppendToTop(true);
+    $fields->addFieldToTab('Root.Config', HeaderField::create('ListItemsDesc')->setTitle('List Items Options'));
+    $fields->addFieldToTab('Root.Config', CheckboxField::create('AlphabeticalOrder')->setTitle('Alphabetical Order for List Items')->setDescription('Show in alphabetical order (By the list item title, overwrites drag and drop order)'));
+    $fields->addFieldToTab('Root.Config', CheckboxField::create('ToggleEffectItems')->setTitle('Toggle Effect for List Items')->setDescription('Set toggle effect on list items'));
         $fields->addFieldToTab("Root.Config", SliderField::create("PhotoMaxWidth","Max Photo Width",50,1600)->setDescription('For photo that is associated with list items'));
-        $fields->addFieldToTab('Root.Config', HeaderField::create('ListCategoriesDesc')->setTitle('List Categories Options'));
-        $fields->addFieldToTab('Root.Config', CheckboxField::create('AlphaOrderCategories')->setTitle('Alphabetical Order for List Categories')->setDescription('Show in alphabetical order (By the list category title, overwrites drag and drop ordering)'));
-        $fields->addFieldToTab('Root.Config', CheckboxField::create('ToggleEffect')->setTitle('Toggle Effect for List Cateogries')->setDescription('Set toggle effect on list categories'));
-        $fields->addFieldToTab('Root.Config', CheckboxField::create('StartToggleClosed')->setTitle('Start Toggle Closed for List Categories')->setDescription('The toggle for all catogries will start closed, if off the first category toggle will be open'));
-        return $fields;
-    }   
+    $fields->addFieldToTab('Root.Config', HeaderField::create('ListCategoriesDesc')->setTitle('List Categories Options'));
+    $fields->addFieldToTab('Root.Config', CheckboxField::create('AlphaOrderCategories')->setTitle('Alphabetical Order for List Categories')->setDescription('Show in alphabetical order (By the list category title, overwrites drag and drop ordering)'));
+    $fields->addFieldToTab('Root.Config', CheckboxField::create('ToggleEffect')->setTitle('Toggle Effect for List Cateogries')->setDescription('Set toggle effect on list categories'));
+    $fields->addFieldToTab('Root.Config', CheckboxField::create('StartToggleClosed')->setTitle('Start Toggle Closed for List Categories')->setDescription('The toggle for all catogries will start closed, if off the first category toggle will be open'));
+    return $fields;
+  }   
 
 }
 
 class ListPage_Controller extends Page_Controller {
 
-    public static function load_requirements() {
-        Requirements::css("listpage/css/listpage.css");
-        Requirements::javascript(FRAMEWORK_DIR .'/thirdparty/jquery/jquery.js');
-        Requirements::javascript("listpage/js/functionality.listpage.js");
-    }
+  public static function load_requirements() {
+    Requirements::css("listpage/css/listpage.css");
+    Requirements::javascript(FRAMEWORK_DIR .'/thirdparty/jquery/jquery.js');
+    Requirements::javascript("listpage/js/functionality.listpage.js");
+  }
 
-    public function init() {
-        parent::init();
-        self::load_requirements();
-    }
+  public function init() {
+    parent::init();
+    self::load_requirements();
+  }
 
-    public function ListCategories() {
-        $listcategoriesfiltered = new ArrayList();
-        if($this->AlphaOrderCategories)
-            $listcategories = $this->getComponents('ListCategories')->sort('Category ASC');
-        else
-            $listcategories = $this->getComponents('ListCategories');
-        if($listcategories) {
-            foreach($listcategories AS $listcategory) {
-                if($listcategory->getComponents('ListItems')->count() > 0) {
-                    $listcategoriesfiltered->push($listcategory); 
-                }
-            }
+  public function ListCategories() {
+    $listcategoriesfiltered = new ArrayList();
+    if($this->AlphaOrderCategories)
+      $listcategories = $this->getComponents('ListCategories')->sort('Category ASC');
+    else
+      $listcategories = $this->getComponents('ListCategories');
+    if($listcategories) {
+      foreach($listcategories AS $listcategory) {
+        if($listcategory->getComponents('ListItems')->count() > 0) {
+          $listcategoriesfiltered->push($listcategory); 
         }
-        return $listcategoriesfiltered;
+      }
     }
+    return $listcategoriesfiltered;
+  }
 
-    public function UncategorizedListItems() {
-        $uncategorizedlistitems = new ArrayList();
-        if($this->AlphabeticalOrder)
-            $listitems = $this->getComponents('ListItems')->sort("Title ASC");
-        else 
-            $listitems = $this->getComponents('ListItems');
-        if($listitems) {
-            foreach($listitems AS $listitem) {
-                if($listitem->Category() == "Other") {
-                    $uncategorizedlistitems->push($listitem); 
-                }
-            }
+  public function UncategorizedListItems() {
+    $uncategorizedlistitems = new ArrayList();
+    if($this->AlphabeticalOrder)
+      $listitems = $this->getComponents('ListItems')->sort("Title ASC");
+    else 
+      $listitems = $this->getComponents('ListItems');
+    if($listitems) {
+      foreach($listitems AS $listitem) {
+        if($listitem->Category() == "Other") {
+          $uncategorizedlistitems->push($listitem); 
         }
-        return $uncategorizedlistitems;
+      }
     }
+    return $uncategorizedlistitems;
+  }
 
-    public function MoreThanOneListCategory() {
-        if($this->ListCategories()->count())
-            return true;
-    }
+  public function MoreThanOneListCategory() {
+    if($this->ListCategories()->count())
+      return true;
+  }
 
-    public function ToggleEffect() {
-        if($this->ToggleEffect == true AND $this->ListCategories()->count() > 0)
-            return true;
-    }
+  public function ToggleEffect() {
+    if($this->ToggleEffect == true AND $this->ListCategories()->count() > 0)
+      return true;
+  }
     
 }
